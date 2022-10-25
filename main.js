098073194de92f97ab7908b728b17475327a06e1
sserges <agounkeserge@gmail.com>
2022-10-25 16:56:29 -0400

    Show Devtools

--- main.js
+++ main.js
@@ -2,14 +2,20 @@ const path = require('path')
 
 const { app, BrowserWindow } = require('electron')
 
+const isDev = process.env.NODE_ENV !== 'production'
 const isMac = process.platform === 'darwin'
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
-    width: 500,
+    width: isDev ? 1000 : 500,
     height: 600,
   })
+
+  // Open devtools if in dev env
+  if (isDev) {
+    mainWindow.webContents.openDevTools()
+  }
 
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
