bcb732dba4554576b91464bdc45aabc6dce5263a
sserges <agounkeserge@gmail.com>
2022-10-30 15:55:22 -0400

    Menu Customization

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const path = require('path')
 
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
 const isDev = process.env.NODE_ENV !== 'production'
 const isMac = process.platform === 'darwin'
 
+// Create the main window
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
     title: 'Image Resizer',
@@ -20,8 +21,13 @@ function createMainWindow() {
   mainWindow.loadFile(path.join(__dirname, './renderer/index.html'))
 }
 
+// App is ready
 app.whenReady().then(() => {
   createMainWindow()
+
+  // Implement menu
+  const mainMenu = Menu.buildFromTemplate(menu)
+  Menu.setApplicationMenu(mainMenu)
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -29,6 +35,21 @@ app.whenReady().then(() => {
     }
   })
 })
+
+// Menu template
+const menu = [
+  {
+    role: 'fileMenu',
+    // label: 'File',
+    // submenu: [
+    //   {
+    //     label: 'Quit',
+    //     click: () => app.quit(),
+    //     accelerator: 'CmdOrCtrl+W',
+    //   },
+    // ],
+  },
+]
 
 app.on('window-all-closed', () => {
   if (!isMac) {
