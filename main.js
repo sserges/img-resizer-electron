3b49520d16921cc8f847b91ef45bcaa6699e086f
sserges <agounkeserge@gmail.com>
2022-11-10 19:42:41 -0400

    IPC Main (Recieve)

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const path = require('path')
 
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow, Menu, ipcMain } = require('electron')
 
 const isDev = process.env.NODE_ENV !== 'production'
 const isMac = process.platform === 'darwin'
@@ -84,6 +84,11 @@ const menu = [
       ]
     : []),
 ]
+
+// Respond to ipcRenderer resize
+ipcMain.on('image:resize', (e, options) => {
+  console.log(options)
+})
 
 app.on('window-all-closed', () => {
   if (!isMac) {
